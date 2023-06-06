c6193d60ba54e7be615265c060b3f1627f7fc28f
DESKTOP-AO5DABD\ryan <ryanenclona@gmail.com>
2023-06-06 22:43:06 +0800

    Make an ActionInterpreter base class

---
+++ Assets/Scripts/ActionInterpreter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.InputSystem;
+
+public abstract class ActionInterpreter
+{
+    protected CustomInputEvents InputEvents;
+    protected string ActionName;
+
+    public ActionInterpreter(CustomInputEvents inputEvents, string actionNameToInterpret)
+    {
+        InputEvents = inputEvents;
+        ActionName = actionNameToInterpret;
+    }
+
+    public void StartListening()
+    {
+        InputEvents.RegisterOnStartPerformedAndCancelled(ActionName, OnInput);
+    }
+
+    public void StopListening()
+    {
+        InputEvents.UnregisterOnStartPerformedAndCancelled(ActionName, OnInput);
+    }
+
+    protected abstract void OnInput(InputAction.CallbackContext context);
+
+}
