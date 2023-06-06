0b6508fba7ef48b9a6d51adaa09b9c34f23bbfd1
DESKTOP-AO5DABD\ryan <ryanenclona@gmail.com>
2023-06-06 22:24:18 +0800

    Add a simple player script that moves using keyboard in the scene

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] CustomInputEvents InputEvents;
+    private Vector2ActionInterpreter MoveInterpreter;
+
+    private void Awake()
+    {
+        
+    }
+    private void OnEnable()
+    {
+        MoveInterpreter = new Vector2ActionInterpreter(InputEvents, PlayerActionMap.Move);
+        MoveInterpreter.StartListening();
+    }
+
+    private void OnDisable()
+    {
+        MoveInterpreter.StopListening();
+    }
+
+    private void Update()
+    {
+        MovePlayer(MoveInterpreter.GetVector2Direction());
+    }
+
+    private void MovePlayer(Vector2 direction)
+    {
+        gameObject.transform.Translate(direction * Time.deltaTime);
+        Debug.Log("translating to direction: " + direction.x *Time.deltaTime);
+    }
+}
