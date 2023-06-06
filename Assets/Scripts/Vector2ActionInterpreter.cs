acd702e8b8189771216c8e587e7dfa08ce5e12d9
DESKTOP-AO5DABD\ryan <ryanenclona@gmail.com>
2023-06-06 22:09:59 +0800

    Make Vector2ActionInterpreter script

---
+++ Assets/Scripts/Vector2ActionInterpreter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Vector2ActionInterpreter
+{
+    private CustomInputEvents InputEvents;
+    private string ActionName = PlayerActionMap.Move;
+
+    private Vector2 InputDirection;
+
+    public Vector2ActionInterpreter(CustomInputEvents inputEvents, string actionNameToInterpret)
+    {
+        InputEvents = inputEvents;
+        ActionName = actionNameToInterpret;
+
+        StartListening();
+    }
+
+    public Vector2 GetVector2Direction()
+    {
+        return InputDirection;
+    }
+
+    private void OnInput(InputAction.CallbackContext context)
+    {
+        InputDirection = context.ReadValue<Vector2>(); 
+    }
+
+    private void StartListening()
+    {
+        InputEvents.RegisterOnStartPerformedAndCancelled(ActionName, OnInput);
+    }
+
+    private void StopListening()
+    {
+        InputEvents.UnregisterOnStartPerformedAndCancelled(ActionName, OnInput);
+
+    }
+}
